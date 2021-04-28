e210cb779b262ee09df3470a0f14a84381db73cd
anastasia <anastasia.nekrasova@jetbrains.com>
2021-04-28 11:07:10 +0300

    update

--- second_mode.c
+++ second_mode.c
@@ -4,37 +4,34 @@ void handle_command(char *command, char *path, char *outPath, int *flag, FileSys
   if (strcmp(command, "exit") == 0) {
     *flag = 0;
   } else if (strcmp(command, "help") == 0) {
-    help();
-  } else if (strcmp(command, "ls") == 0) {
-    ls(fileSystem, path);
-  } else if (strcmp(command, "pwd") == 0) {
-    pwd(fileSystem);
-  } else if (strcmp(command, "cd") == 0) {
-    cd(fileSystem, path);
+      help();
   } else if (strcmp(command, "cp") == 0) {
-    cp(fileSystem, path, outPath);
+      cp(fileSystem, path, outPath);
+  } else if (strcmp(command, "ls") == 0) {
+      ls(fileSystem, path);
+  } else if (strcmp(command, "cd") == 0) {
+      cd(fileSystem, path);
+  } else if (strcmp(command, "pwd") == 0) {
+      pwd(fileSystem);
   } else {
-    printf("Invalid command. Enter 'help'\n");
+      printf("Invalid command. Enter 'help'\n");
   }
 }
 
 int second_mode(char *filePath) {
     FileSystem *fileSystem = openFileSystem(filePath);
     if (fileSystem != NULL) {
-        int flag = 1;
+        int flag = 1; //to stop work
         char *inputString = malloc(1024);
         while (flag) {
-            printf("> ");
-            fgets(inputString, 1024, stdin);
-            char *command = strtok(inputString, " \n");
-            if (command == NULL) {
-                continue;
-            }
+          printf("> ");
+          fgets(inputString, 1024, stdin);
+          char *command = strtok(inputString, " \n");
+          if (command != NULL) {
             char *path = strtok(NULL, " \n");
             char *outPath = strtok(NULL, " \n");
             handle_command(command, path, outPath, &flag, fileSystem);
-
-            
+          }
         }
         closeFileSystem(fileSystem);
         return 0;
